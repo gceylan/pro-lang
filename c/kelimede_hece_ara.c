bf8aba9402f64f270753718c93cd2dbecb80379c
gökhan <gokhan.ceylan@bil.omu.edu.tr>
2011-11-20 00:10:58 +0200

    hece var mi?

--- c/kelimede_hece_ara.c
+++ c/kelimede_hece_ara.c
@@ -1,19 +1,23 @@
+// stringde hece arama
+
 #include <stdio.h>
 #include <string.h>
 
 int hece_ara(char* kelime, char* hece) {
-	char* p1 = kelime;
-	char* p2 = hece;
+	char* p1 = kelime;       //kelimeyi goster
+	char* p2 = hece;         //heceyi goster
 	int i = 0, j = 0;
 
 	for(i=0;i < strlen(kelime); i++) {
-		if(*(p1 + i) == *(p2 + j)) {
-			for(j=0; j < strlen(hece); j++) {
-				if(*(p1 + i + j) != *(p2 + j))
+		if(*(p1 + i) == *(p2 + j)) {                    //kelime stringinin i. elemanı hece stringinin 0. elemanına eşit mi?
+			for(j=0; j < strlen(hece); j++) {           //ilk harfler uyustuguna gore diger harflere bakalım...
+				if(*(p1 + i + j) != *(p2 + j))          //eger bir uyusmayan varsa dogru degildir.
 					return 0;
 			}
 		}
 	}
+	if (j == 0)       //eger j degiskeni 0 ise kelime stringinin icinde aradıgım hecenin bas harfi yoktur. ornek: gceylan xc
+		return 0;
 	return 1;
 }
 
@@ -24,7 +28,7 @@ int main(void) {
 	printf("kelime --> aranacak ifade:(ör: gceylan gc) ");
 	scanf("%s%s", kelime, hece);
 
-	if (hece_ara(kelime, hece))
+	if (hece_ara(kelime, hece) == 1)
 		printf("True");
 	else
 		printf("False");
