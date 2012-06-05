d0b65a51e304742fdc9524b8bcb5e60112abe255
gceylan <gokhan.ceylan@bil.omu.edu.tr>
2012-06-05 16:09:06 +0300

    hatalı güncelleme

---
+++ java/Ajanda/src/JLayerDemo.java
@@ -0,0 +1,31 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
+
+
+public class JLayerDemo {
+	
+	String path;
+	Player p;
+	
+	public JLayerDemo() throws JavaLayerException {
+		try {
+			path = "sound/Wake_Up_Guitar.mp3";
+			FileInputStream file = new FileInputStream(path);
+			p = new Player(file);
+		
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void oynat() throws JavaLayerException {
+		p.play();
+	}
+	
+	public void kapat() {
+		p.close();
+	}
+}
